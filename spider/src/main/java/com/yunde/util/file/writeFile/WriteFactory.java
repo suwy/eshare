0df00665bda4ae83612d8657c01b7db87b0adf0e
suwy <suwy2013@gmail.com>
2018-10-12 17:47:25 +0800

    要换方法，耗的时间太久了

--- spider/src/main/java/com/yunde/util/file/writeFile/WriteFactory.java
+++ spider/src/main/java/com/yunde/util/file/writeFile/WriteFactory.java
@@ -1,13 +1,14 @@
 package com.yunde.util.file.writeFile;
 
-import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.io.*;
 
 /**
  * Created by laisy on 2018/8/22.
  */
 public class WriteFactory {
 
+    //要换方法，耗的时间太久了
+    @Deprecated
     public static void write2File(String fileName, String content) {
         try {
             // 打开一个随机访问文件流，按读写方式
@@ -21,5 +22,38 @@ public class WriteFactory {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        FileOutputStream outSTr = null;
+        BufferedOutputStream Buff = null;
+        int count = 1000;//写文件行数
+        long begin0 = System.currentTimeMillis();
+        try {
+            File file = new File("D:/lzda/json/"+"gbxx.json");
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+            }
+//            file.createNewFile();
+            outSTr = new FileOutputStream(file);
+            Buff = new BufferedOutputStream(outSTr);
+            for (int i = 0; i < count; i++) {
+                Buff.write("测试java 文件操作\r\n".getBytes());
+            }
+            Buff.flush();
+            Buff.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                Buff.close();
+                outSTr.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        long end0 = System.currentTimeMillis();
     }
+
+
 }
